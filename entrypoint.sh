7fe6afcf9c1a1046efc0847798740d8c71e941b5
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2025-12-04 21:51:40 +0100

    remove reload

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cd /app || exit
 export FLASK_APP=app
 export FLASK_ENV=development
 export FLASK_RUN_PORT=8000
-uv run gunicorn app:app --bind 0.0.0.0:8000 --reload --timeout 120 --workers 4
+uv run gunicorn app:app --bind 0.0.0.0:8000 --timeout 120 --workers 4
